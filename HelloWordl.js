4be4e4c27e91f9896655762628b075ca050fa285
kd1995-max <k.d1995@outlook.com>
2021-07-04 01:14:54 +0530

    tracked files

--- HelloWordl.js
+++ HelloWordl.js
@@ -3,5 +3,5 @@ const numbers = [1 , 2 , 3];
 const array = [ ...numbers , 4];
 for (let index = 0; index < array.length; index++) {
     console.log(array[index]);
-    //hello
+    //hello world
 }
